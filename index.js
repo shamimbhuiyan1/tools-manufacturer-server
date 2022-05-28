3b89d1c4dc7c7182c1f2b23078e6495204b86a9c
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-05-28 13:09:46 +0600

    mongodb connented

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
@@ -9,8 +10,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri =
+  "mongodb+srv://tools_admin:<password>@cluster0.xcot7.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 async function run() {
   try {
+    await client.connect();
+    const toolsCollection = client.db("car_tools").collection("tools");
+    app.get("/tools", async (req, res) => {
+      const query = {};
+      const cursor = toolsCollection.find(query);
+      const tools = await cursor.toArray();
+      res.send(tools);
+    });
   } finally {
   }
 }
